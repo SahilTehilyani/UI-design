833c611daf595d1d64c5d8631ca9ff41d12714e6
SahilTehilyani <167900474+SahilTehilyani@users.noreply.github.com>
2024-12-12 15:12:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+    <div className="box">
+    <p id="pge">STRENGTH</p>
+    <img  src="https://www.hussle.com/blog/wp-content/uploads/2021/03/shutterstock_1012770097-min-980x653.jpg" alt='error'/>
+    <p id="pge2">MOBILITY</p>
+    <img  src="https://cdn.prod.website-files.com/669988e9f57d1b262adfb8dd/670633b6bd4bacabf3a2c57b_67063070c68e650e67326137_9%252023%252024-min.jpeg" alt='error'/>
+    <p id="pge3">DRILLS</p>
+    <img  src="https://img.freepik.com/free-photo/beautiful-scenery-road-forest-with-lot-colorful-autumn-trees_181624-30942.jpg" alt='error'/>
+</div>
+</div>
+  );
+}
+
+export default App;
+ 
